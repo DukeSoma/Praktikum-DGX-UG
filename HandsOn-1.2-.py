033632966879f7fdbfe92423c5600bfa0c8d3e7a
Ai Hoshino <133128378+DukeSoma@users.noreply.github.com>
2023-05-22 06:31:33 +0700

    Create HandsOn-1.2-.py

---
+++ HandsOn-1.2-.py
@@ -0,0 +1,21 @@
+# Author: @makanyakuliah
+# DO NOT SELL THIS CODE, PLEASE?
+# Petunjuk : Ganti kata sesuai dengan kemauan Anda, lalu jalankan di Google Colab
+
+import asyncio
+
+async def gabung_kata(kata):
+    return ' '.join(kata).title()
+
+async def hitung_karakter(kalimat):
+    return sum(not c.isspace() for c in kalimat)
+
+async def main():
+    kata = ['saya', 'suka', 'ai', 'hoshino']
+    hasil = await gabung_kata(kata)
+    print(hasil)
+    
+    jumlah_karakter = await hitung_karakter(hasil)
+    print(f'Jumlah karakter (tanpa spasi): {jumlah_karakter}')
+
+await main()
