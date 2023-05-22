6f8f55d770d2c57d576bc32e09227dec6c18546b
Ai Hoshino <133128378+DukeSoma@users.noreply.github.com>
2023-05-22 08:33:15 +0700

    Update HandsOn-1.1-.py

--- HandsOn-1.1-.py
+++ HandsOn-1.1-.py
@@ -8,6 +8,7 @@ async def gabung_kata(kata):
     return ' '.join(kata).title()
 
 async def main():
+    # Ganti kata di bawah tanpa menghilangkan tanda ' '
     kata = ['saya', 'suka', 'ai', 'hoshino']
     hasil = await gabung_kata(kata)
     print(hasil)
